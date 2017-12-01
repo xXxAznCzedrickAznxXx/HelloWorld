54324b6a94fb2fd17dbc7ccd93a502a599e5bf0b
zomgTaco <OmaeWaMouShindeiruZOMGNANITACO@github.com>
2017-12-01 13:48:22 -0700

    in the middle of 8

--- Program.cs
+++ Program.cs
@@ -7,7 +7,20 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello!"));
+            float number1 = float.Parse(Console.ReadLine());
+            float number2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Select an option:\n1.-Add\n2.-Subtract\n3.-Multiply\n4.-Divide\n5.Exit");
+
+            int option = int.Parse(Console.ReadLine())
         }
+
+        public static float AddTwoNumbers(float n1, float n2)
+        {
+            float result = n1 + n2;
+
+            return result;
+        }
+
     }
 }
